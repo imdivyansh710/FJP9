2124c656a5efed1738b08543a74f83776d863265
imdivyansh710 <imdivyansh710@gmail.com>
2022-08-18 11:20:53 +0530

    filter

--- Browser/Camera/script.js
+++ Browser/Camera/script.js
@@ -1,112 +1,133 @@
-let recordBtn=document.querySelector(".record-btn");
-let recordBtnCont=document.querySelector(".record-btn-cont");
-let captureBtn=document.querySelector(".capture-btn");
-let captureBtnCont=document.querySelector(".capture-btn-cont");
-let timerCont=document.querySelector(".timer-cont");
-let timer=document.querySelector(".timer");
-let video=document.querySelector("video");
-
-let constraints={
-    video:true,
-    audio:true,
-};
-
+let recordBtn=document.querySelector(".record-btn")
+let recordBtnCont=document.querySelector(".record-btn-cont")
+let captureBtn=document.querySelector(".capture-btn")
+let captureBtnCont=document.querySelector(".capture-btn-cont")
+let timerCont = document.querySelector(".timer-cont");
+let timer = document.querySelector(".timer");
+let video = document.querySelector("video");
+let filterColor = "transparent";
+let constraints = {
+    video: true,
+    audio: true
+}
 let mediaRecorder;
-// this will store the video mediastream
-let chunks=[];
-navigator.mediaDevices.getUserMedia(constraints).then(
-    (stream)=>{
-        video.srcObject=stream;
+//this will store the video mediastream 
+let chunks = [];
+navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    video.srcObject = stream;
 
+    mediaRecorder = new MediaRecorder(stream);
 
-        mediaRecorder=new MediaRecorder(stream);
+    mediaRecorder.addEventListener("start", () => {
+        console.log("rec started");
+    });
+    mediaRecorder.addEventListener("dataavailable", (e) => {
+        chunks.push(e.data); 
+        //single blob of video created
+        
+    })
 
-        mediaRecorder.addEventListener("start",()=>{
-            console.log("rec started");
-        });
+     mediaRecorder.addEventListener("stop", () => {
+         console.log("rec stopped");
+         let blob = new Blob(chunks, { type: "video/mp4" });
+         let videoURL = URL.createObjectURL(blob);
+         console.log(videoURL);
 
-        mediaRecorder.addEventListener("dataavailable",(e) =>{
-            console.log("data available");
-            chunks.push(e.data);
-            //single blob of object created
-        });
+         let a = document.createElement("a");
+         a.href = videoURL;
+         a.download="myVideo.mp4"
+         a.click();
+     });
+})
 
-        mediaRecorder.addEventListener("stop",()=>{
-            console.log("rec stopped");
-            let blob=new Blob(chunks,{type: "video/mp4"});
-            let videoURL=URL.createObjectURL(blob);
-            console.log(videoURL);
-
-            let a=document.createElement("a");
-            a.href=videoURL;
-            a.download="myVideo.mp4";
-            a.click();
-        });
-    }
-);
-
-let isRecording=false;
-recordBtnCont.addEventListener("click",function(){
-    if(!isRecording){
-        // we have to record
+let isRecording = false;
+recordBtnCont.addEventListener("click", function () {
+    if (!isRecording) {
+        //we have to record 
         mediaRecorder.start();
         startTimer();
         recordBtn.classList.add("scale-record");
-        timer.style.display="block";
-    }
-    else{
-        // stop the recording
+        timer.style.display = "block";
+    } else {
+        //stop the recording 
         mediaRecorder.stop();
         stopTimer();
         recordBtn.classList.remove("scale-record");
-        timer.style.display="none";
+        timer.style.display = "none";
     }
-    isRecording=!isRecording;
+    
+    isRecording = !isRecording;
+  
 });
 
+let counter = 0;
+let timerID;
+function startTimer() {
+    timer.style.display = "block";
+    function displayTimer() {
+        let totalSeconds = counter;
 
+        let hours = Number.parseInt(totalSeconds / 3600);
+        totalSeconds = totalSeconds % 3600;
 
+        let minutes = Number.parseInt(totalSeconds / 60);
+        totalSeconds = totalSeconds % 60;
 
-let counter=0;
-let TimerID;
-function startTimer(){
-    timer.style.display="block";
-    function displayTimer(){
-        let totalSeconds=counter;
+        let seconds = totalSeconds;
 
-        let hours=Number.parseInt(totalSeconds/3600);
-        totalSeconds=totalSeconds%3600;
+        hours = hours < 10 ? `0${hours}` : hours;
+        minutes = minutes < 10 ? `0${minutes}` : minutes;
+        seconds = seconds < 10 ? `0${seconds}` : seconds;
 
-        let minutes=Number.parseInt(totalSeconds/60);
-        totalSeconds=totalSeconds%60;
-
-        let seconds=totalSeconds;
-
-        hours=hours<10?`0${hours}`:hours;
-        minutes=minutes<10?`0${minutes}`:minutes;
-        seconds=seconds<10?`0${seconds}`:seconds;
-
-        timer.innerText=`${hours}:${minutes}:${seconds}`;
-        counter++;
+        timer.innerText = `${hours}:${minutes}:${seconds}`;
+         counter++;
     }
-    TimerID=setInterval(displayTimer,1000);
+    timerID=setInterval(displayTimer, 1000);
+
 }
 
-function stopTimer(){
-    clearInterval(TimerID);
-    timer.innerText="00:00:00";
-    timer.style.display="none";
+function stopTimer() {
+    clearInterval(timerID);
+    timer.innerText = "00:00:00";
+    timer.style.display = "none";
+    counter=0;
 }
 
 
 
-captureBtnCont.addEventListener("click",function(){
+captureBtnCont.addEventListener("click", function(){
     captureBtn.classList.add("scale-capture");
-    setTimeout(()=>{
-        captureBtn.classList.remove("scale-capture");
-    },1000);
+    //canvas 
+    let canvas = document.createElement("canvas");
+    let ctx = canvas.getContext("2d");
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    
+    ctx.fillStyle = filterColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+
+
+    let image = canvas.toDataURL("image/jpeg");
+
+     let a = document.createElement("a");
+     a.href = image;
+     a.download = "myPic.jpeg";
+     a.click();
+
+    setTimeout(() => {
+        captureBtn.classList.remove("scale-capture")
+    },1000)
+})
+
+let allFilters = document.querySelectorAll(".filter");
+let filterLayer = document.querySelector(".filter-layer");
+
+allFilters.forEach((filterEle) => {
+    filterEle.addEventListener("click", () => {
+        filterColor = window.getComputedStyle(filterEle).getPropertyValue('background-color');
+        filterLayer.style.backgroundColor = filterColor;
+    });
 });
-
-
-
-
